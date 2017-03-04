2dbc07a3605ec33f6784bf0ad2230c65e60f33bb
Giulia <giuliapenna88@gmail.com>
2017-03-04 14:10:09 +0100

    added color field to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302152452) do
+ActiveRecord::Schema.define(version: 20170303174144) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.string   "colour"
+    t.decimal  "price",       precision: 10, scale: 2
   end
 
 end
