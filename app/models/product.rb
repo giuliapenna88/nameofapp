7e6668dd308c8c8ed7de5975fe4c0d012b0f75c2
Giulia <giuliapenna88@gmail.com>
2017-03-14 16:10:43 +0100

    5.2 fix search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,14 @@
 class Product < ApplicationRecord
   has_many :orders
   def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
-	end
+      app_name = "redacted"
+      if Rails.env.development?
+  				Product.where("name LIKE ?", "%#{search_term}%")
+      elsif Rails.env.production?
+      	  Product.where("name LIKE ?", "%#{search_term}%")
+      end
+    end
 end
 
+
+		
