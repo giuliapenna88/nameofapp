3eaf2e2eddfcb49a64734f031209ae9a4dc0b57a
Giulia <giuliapenna88@gmail.com>
2017-03-25 10:34:51 +0100

    5.11 create a welcome email for new sign up users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,15 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "giulia@fahrradkit.com"
 
   def contact_form(email, name, message)
   @message = message
     mail(:from => email,
         :to => 'giuliapenna88@gmail.com',
         :subject => "A new contact form message from #{name}")
+  end
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
