6e8b3a3e2df44e4a3b37c29072226441d1c04d66
Giulia <giuliapenna88@gmail.com>
2017-03-28 10:11:33 +0200

    5.11 added welcome_email to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,20 +26,18 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-      @user = User.new(params[:user])
+    @user = User.new(user_params)
 
-      respond_to do |format|
-        if @user.save
-          # Tell the UserMailer to send a welcome email after save
-
-          format.html { redirect_to(@user, notice: 'User was successfully created.') }
-          format.json { render json: @user, status: :created, location: @user }
-        else
-          format.html { render action: 'new' }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
+  end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
