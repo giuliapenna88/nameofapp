bdd1c631bbea2a44c4879106f96249e992eb923c
Giulia <giuliapenna88@gmail.com>
2017-04-02 14:22:05 +0200

    6.3 functional test login

---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+
+before do
+    @user = User.create!(email: "example@example.com", password: "testpassed123")
+    @user1 = User.create!(email: "example1@example.com", password: "timeforatest")
+  end
+
+  describe "GET #show" do
+
+    context "User is logged in" do
+      before do
+        it "loads the correct user details" do
+          expect(response).to have_http_status(200)
+          expect(assign(:user)).to eq @user
+        end
+      end
+    end
+
+    context 'No user is logged in' do
+      it 'redirects to login' do
+        get :show, id: @user.id
+      end
+    end
+
+    context 'Cannont access second users show page' do
+       it "redirects to root" do
+         get :show, params: {id: @user.id}
+         redirect_to(root_path)
+       end
+     end
+  end
+
+end
