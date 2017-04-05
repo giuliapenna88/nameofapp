5665a100f4bf6d441514cd8d225cb590e285b531
Giulia <giuliapenna88@gmail.com>
2017-04-05 11:19:39 +0200

    6.9 added elevate zoom to product show page

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,19 @@
-$(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets' , scoreName: 'comment[rating]'});
-    
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+var refreshRating = function() {
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+};
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+  refreshRating();
+
+  $('.img_zoom').ezPlus({
+    zoomType: "lens",
+    lensShape : "round",
+    lensSize    : 200
+  });
 });
