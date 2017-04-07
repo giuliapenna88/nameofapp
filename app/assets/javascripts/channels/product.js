025cbd9e8042fb4a3b791e6d6f08a79b28662c0a
Giulia <giuliapenna88@gmail.com>
2017-04-07 14:29:45 +0200

    Zoom

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -22,6 +22,5 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
       });
     }
 
-      $(document).on('turbolinks:load', function() {
         App.product.listen_to_comments();
-    });
+    
